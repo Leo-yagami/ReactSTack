da95a0d76ee0a920be59faaa09e122c31bd4bf94
Haitham Abdulhamid <xxharixdragoon@gmail.com>
2024-12-17 19:08:17 +0300

    first commit

---
+++ landing-page1/src/Components/Nav.jsx
@@ -0,0 +1,109 @@
+
+
+export default function Nav(){
+    return(
+        <>
+            {/* The navigation is split into two: the left side with the logo */}
+            {/* Thr right side with the account and setting */}
+            {/* Main wrapper */}
+            {/* Colors */}
+            <div className="bg-[#032C47] p-4 pb-6 relative flex justify-between text-white text-lg">
+                {/* Logo + nav links */}
+                <div className="flex items-center">
+                    {/* Logo */}
+                    <div className="mr-12">
+                        {/* Insert logo here */}
+                        <h1 className="font-serif font-extrabold rounded-lg px-1 py-1.5 hover:text-white text-bge text-2xl"><span className="text-[#F86709]">U</span>TRADIE</h1>
+                    </div>
+                    {/* Divider line */}
+                    <div className="bg-white w-0.5 h-full"></div>
+                    {/* Nav links */}
+                    <nav className="flex gap-8 ml-12 transition-all">
+                        <div className="group relative">
+                            <a href="#" className="rounded-full group-hover:bg-[white] group-hover:text-[#032C47]  px-5 py-2">Growth & Learning<span className="inline-block ml-2 group-hover:rotate-180 group-hover:translate-y-0.5 duration-300">&#x25BC;</span></a>
+                            <div className="absolute left-2 hidden mt-2 space-y-2 bg-white z-20 shadow-lg group-hover:block w-48 rounded-lg p-4">
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>🎥</span> {/* Icon for "Create" */}
+                                    <span>Create</span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>📚</span> {/* Icon for "Learn" */}
+                                    <span>Learn</span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>🎯</span> {/* Icon for "Goals" */}
+                                    <span>Goals</span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="relative group">
+                            <a href="#" className="rounded-full group-hover:bg-[white] group-hover:text-[#032C47] px-5 py-2">Communication & Social<span className="inline-block ml-2 group-hover:rotate-180 group-hover:translate-y-0.5 duration-300">&#x25BC;</span></a>
+                            <div className="absolute z-20 hidden left-4 mt-2 spacy-y-2 bg-white shadow-lg group-hover:block w-52     rounded-lg p-4 ">
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Message</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Connection</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Events</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Competitions</span>
+                                    <span></span>
+                                </a>
+                            </div>
+                        </div>
+                        <div className="relative group">
+                            <a href="#" className="rounded-full  group-hover:bg-[white] group-hover:text-[#032C47] px-5 py-2">Opportunities<span className="inline-block ml-2 group-hover:rotate-180 group-hover:translate-y-0.5 duration-300">&#x25BC;</span></a>
+                            <div className="absolute z-20 hidden left-1 mt-2 spacy-y-2 bg-white shadow-lg group-hover:block w-40 rounded-lg p-4 ">
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Jobs</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Market Place</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Affiliate</span>
+                                    <span></span>
+                                </a>
+                            </div>
+                        </div>
+                    </nav>
+                    
+                </div>
+                {/* Account setting and User name */}
+                <div className="flex items-center pr-4">
+                    <div className="mr-8 group relative">
+                        <a href="#" className="rounded-full  group-hover:bg-[white] group-hover:text-[#032C47] px-5 py-2">Account & Setting<span className="inline-block ml-2 group-hover:rotate-180 group-hover:translate-y-0.5 duration-300">&#x25BC;</span></a>
+                        <div className="absolute z-30 hidden left-2 mt-2 spacy-y-2 bg-white shadow-lg group-hover:block w-48 rounded-lg p-4 ">
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Message</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Connection</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Events</span>
+                                    <span></span>
+                                </a>
+                                <a href="#" className="flex items-center space-x-2 text-gray-800 rounded-lg px-1 py-1.5 hover:text-white hover:bg-[#F86709]">
+                                    <span>Competitions</span>
+                                    <span></span>
+                                </a>
+                            </div>
+                    </div>
+                    <div className="bg-white w-0.5 h-full"></div>
+                    <p className="ml-8">Amir Getnet</p>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ landing-page1/src/Components/hero.jsx
@@ -0,0 +1,42 @@
+import image1 from '../assets/images/1.png';
+import image2 from '../assets/images/2.png';
+import image3 from '../assets/images/3.png';
+import image4 from '../assets/images/4.png';
+import image5 from '../assets/images/5.png';
+ 
+ export default function Hero(){
+    return(
+        <section id="Hero" className="relative flex items-center  gap-16 px-8 bg-gradient-to-b from-white to-[#FEEBDE] min-h-[calc(100vh-4.5rem)]">
+        {/* The hero has two sections, the text and images section */}
+        {/* Text section */}
+        <div className='-mt-16'>
+            <h1 className="text-8xl text-[#000000] font-serif font-bold">Shaping Futures.</h1>
+            <h1 className="text-8xl mt-4 text-[#F86709] font-serif font-bold">Building Trades<span className="text-[#000000]">.</span></h1>
+            <p className="text-3xl max-w-4xl mt-6 ">Empowering the Future of Trades: Connect, Learn, and Build with <span className="text-[#F86709]">U</span>TRADIE - Where Opportunities Meet Skilled Hands</p>
+            {/* Button group */}
+            <div className="mt-8 flex gap-6 text-white">
+                <button className="py-4 px-11 bg-[#032C47] rounded-full text-3xl shadow-xl hover:shadow-2xl active:translate-y-1 duration-300">Sign Up</button>
+                <button className="py-4 px-11 bg-[#E97132] rounded-full text-3xl shadow-xl hover:shadow-2xl active:translate-y-1 duration-300">Log In</button>
+            </div>
+        </div>
+        {/* Hard Part: Images Section */}
+        <div className='relative h-[700px] w-[1000px] mt-16'>
+            <div>
+                <img src={image1} alt="" className='absolute w-[250px] h-[250px] z-20 top-[2rem] left-[10rem] hover:-translate-y-2 duration-300'/>
+            </div>
+            <div className='overflow-hidden'>
+                <img src={image2} alt="" className='absolute w-[550px] h-[550px] z-20 -top-[5.5rem] -right-[1rem] hover:-translate-y-2 duration-300'/>
+            </div>
+            <div>
+                <img src={image3} alt="" className='absolute w-[300px] h-[300px] z-20 bottom-[1rem] left-[8rem] hover:-translate-y-2 duration-300'/>
+            </div>
+            <div>
+                <img src={image4} alt="" className='absolute w-[250px] h-[250px] z-20 right-[11rem] bottom-[0rem] hover:-translate-y-2 duration-300'/>
+            </div> 
+            <div>
+                <img src={image5} alt="" className='absolute w-[450px] h-[450px] z-10 top-[calc(12rem-4.5rem)] left-[calc(20rem-4.5rem)] '/>
+            </div>
+        </div>
+        </section>
+    )
+ }
